57fe024ef690615c48da246b242b875325a42b94
Admin <Admin@ADMINSTRATOR-PC>
2019-06-24 14:02:50 +0800

    修改添加日志时提示资源被占用问题

--- Infrastructure.Seedwork/LogManager.cs
+++ Infrastructure.Seedwork/LogManager.cs
@@ -11,27 +11,30 @@ namespace LMS.Infrastructure.Seedwork
 {
     public class LogManager
     {
+        private static readonly object lockObj = new object();
+
         public static void WriteLog(string sql)
         {
-            string isEnableLog = ConfigurationManager.AppSettings["IsEnableLog"];
-            if(isEnableLog=="1")
+            lock (lockObj)
             {
-                string logPath = HttpContext.Current.Server.MapPath("~")+ ConfigurationManager.AppSettings["LogPath"];
-                if(!Directory.Exists(logPath))
+                string isEnableLog = ConfigurationManager.AppSettings["IsEnableLog"];
+                if (isEnableLog == "1")
                 {
-                    Directory.CreateDirectory(logPath);
-                }
+                    string logPath = HttpContext.Current.Server.MapPath("~") + ConfigurationManager.AppSettings["LogPath"];
+                    if (!Directory.Exists(logPath))
+                    {
+                        Directory.CreateDirectory(logPath);
+                    }
 
-                string logFile = logPath + "/log-" + DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
-                if (!System.IO.File.Exists(logFile))
-                {
-                    FileStream stream = System.IO.File.Create(logFile);
-                    stream.Close();
-                    stream.Dispose();
-                }
-                using (StreamWriter writer = new StreamWriter(logFile, true))
-                {
-                    writer.WriteLine(sql);
+                    string logFile = logPath + "/log-" + DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+                    if (!System.IO.File.Exists(logFile))
+                    {
+                        File.Create(logFile).Dispose();
+                    }
+                    using (StreamWriter writer = new StreamWriter(logFile, true, Encoding.Default))
+                    {
+                        writer.WriteLine(sql);
+                    }
                 }
             }
         }
